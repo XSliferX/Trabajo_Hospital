535026f0771a1b532deb47891fba29d7ee8e890e
XSliferX <70914552+XSliferX@users.noreply.github.com>
2023-11-16 05:18:06 +0100

    Add files via upload

---
+++ Trabajo_Hospital/Trabajo_Hopital/Trabajo_Hopital/Form2.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Trabajo_Hopital
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(270, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 20);
+            label1.TabIndex = 1;
+            label1.Text = "FORMULARIO 2";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(217, 147);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(248, 27);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(217, 202);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(248, 27);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(217, 101);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(248, 27);
+            textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(684, 391);
+            button1.Name = "button1";
+            button1.Size = new Size(104, 47);
+            button1.TabIndex = 7;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
---
+++ Trabajo_Hospital/Trabajo_Hopital/Trabajo_Hopital/FormPrincipal.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Trabajo_Hopital
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            panelContenedor = new Panel();
+            panelformularios = new Panel();
+            panelMenu = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panelBarraTitulo = new Panel();
+            btnRestaurar = new PictureBox();
+            btnMinimizar = new PictureBox();
+            btnMaximizar = new PictureBox();
+            btnCerrar = new PictureBox();
+            panelContenedor.SuspendLayout();
+            panelMenu.SuspendLayout();
+            panelBarraTitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)btnRestaurar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnMinimizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnMaximizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).BeginInit();
+            SuspendLayout();
+            // 
+            // panelContenedor
+            // 
+            panelContenedor.BackColor = SystemColors.ActiveCaption;
+            panelContenedor.Controls.Add(panelformularios);
+            panelContenedor.Controls.Add(panelMenu);
+            panelContenedor.Controls.Add(panelBarraTitulo);
+            panelContenedor.Dock = DockStyle.Fill;
+            panelContenedor.Location = new Point(0, 0);
+            panelContenedor.Name = "panelContenedor";
+            panelContenedor.Size = new Size(1024, 650);
+            panelContenedor.TabIndex = 0;
+            // 
+            // panelformularios
+            // 
+            panelformularios.BackColor = SystemColors.Control;
+            panelformularios.Dock = DockStyle.Fill;
+            panelformularios.Location = new Point(250, 40);
+            panelformularios.Name = "panelformularios";
+            panelformularios.Size = new Size(774, 610);
+            panelformularios.TabIndex = 2;
+            // 
+            // panelMenu
+            // 
+            panelMenu.BackColor = Color.FromArgb(4, 41, 68);
+            panelMenu.Controls.Add(button3);
+            panelMenu.Controls.Add(button2);
+            panelMenu.Controls.Add(button1);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 40);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(250, 610);
+            panelMenu.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(3, 210);
+            button3.Name = "button3";
+            button3.Size = new Size(250, 47);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(0, 157);
+            button2.Name = "button2";
+            button2.Size = new Size(250, 47);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(0, 104);
+            button1.Name = "button1";
+            button1.Size = new Size(250, 47);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // panelBarraTitulo
+            // 
+            panelBarraTitulo.BackColor = Color.FromArgb(13, 93, 142);
+            panelBarraTitulo.Controls.Add(btnRestaurar);
+            panelBarraTitulo.Controls.Add(btnMinimizar);
+            panelBarraTitulo.Controls.Add(btnMaximizar);
+            panelBarraTitulo.Controls.Add(btnCerrar);
+            panelBarraTitulo.Dock = DockStyle.Top;
+            panelBarraTitulo.Location = new Point(0, 0);
+            panelBarraTitulo.Name = "panelBarraTitulo";
+            panelBarraTitulo.Size = new Size(1024, 40);
+            panelBarraTitulo.TabIndex = 0;
+            panelBarraTitulo.MouseMove += panelBarraTitulo_MouseMove;
+            // 
+            // btnRestaurar
+            // 
+            btnRestaurar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnRestaurar.Cursor = Cursors.Hand;
+            btnRestaurar.Image = (Image)resources.GetObject("btnRestaurar.Image");
+            btnRestaurar.Location = new Point(974, 12);
+            btnRestaurar.Name = "btnRestaurar";
+            btnRestaurar.Size = new Size(16, 16);
+            btnRestaurar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnRestaurar.TabIndex = 3;
+            btnRestaurar.TabStop = false;
+            btnRestaurar.Visible = false;
+            btnRestaurar.Click += btnRestaurar_Click;
+            // 
+            // btnMinimizar
+            // 
+            btnMinimizar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnMinimizar.Cursor = Cursors.Hand;
+            btnMinimizar.Image = (Image)resources.GetObject("btnMinimizar.Image");
+            btnMinimizar.Location = new Point(952, 12);
+            btnMinimizar.Name = "btnMinimizar";
+            btnMinimizar.Size = new Size(16, 16);
+            btnMinimizar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnMinimizar.TabIndex = 2;
+            btnMinimizar.TabStop = false;
+            btnMinimizar.Click += btnMinimizar_Click;
+            // 
+            // btnMaximizar
+            // 
+            btnMaximizar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnMaximizar.Cursor = Cursors.Hand;
+            btnMaximizar.Image = (Image)resources.GetObject("btnMaximizar.Image");
+            btnMaximizar.Location = new Point(974, 12);
+            btnMaximizar.Name = "btnMaximizar";
+            btnMaximizar.Size = new Size(16, 16);
+            btnMaximizar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnMaximizar.TabIndex = 1;
+            btnMaximizar.TabStop = false;
+            btnMaximizar.Click += btnMaximizar_Click;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnCerrar.Cursor = Cursors.Hand;
+            btnCerrar.Image = (Image)resources.GetObject("btnCerrar.Image");
+            btnCerrar.Location = new Point(996, 12);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(16, 16);
+            btnCerrar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnCerrar.TabIndex = 0;
+            btnCerrar.TabStop = false;
+            btnCerrar.Click += btnCerrar_Click;
+            // 
+            // FormPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1024, 650);
+            Controls.Add(panelContenedor);
+            FormBorderStyle = FormBorderStyle.None;
+            MinimumSize = new Size(650, 400);
+            Name = "FormPrincipal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panelContenedor.ResumeLayout(false);
+            panelMenu.ResumeLayout(false);
+            panelBarraTitulo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)btnRestaurar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnMinimizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnMaximizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelContenedor;
+        private Panel panelBarraTitulo;
+        private Panel panelformularios;
+        private Panel panelMenu;
+        private PictureBox btnMinimizar;
+        private PictureBox btnMaximizar;
+        private PictureBox btnCerrar;
+        private PictureBox btnRestaurar;
+        private Button button1;
+        private Button button3;
+        private Button button2;
+    }
+}
